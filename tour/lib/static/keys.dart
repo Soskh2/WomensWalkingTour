ec87ca85da175cf38e0b8b2b9cd618092d982bd6
Soskh2 <sofia.khemani@yale.edu>
2025-04-06 19:52:23 -0400

    remove keys

--- tour/lib/static/keys.dart
+++ tour/lib/static/keys.dart
@@ -1,7 +1,9 @@
+// These API keys must be provided in order to use the app. 
+
 class Keys {
   Keys._();
 
-  static const airtable = "patnOVix5R5wsTz8C.92580deda957e6f6da50f28ad387509eefc23c22ab1ed9afe2efed4ef0e33818";
-  static const googleMaps = "AIzaSyC7VRiXgUK1LyZ18NcbDe9lOZ922IBFbbk";
+  static const airtable = "";
+  static const googleMaps = "";
 
 }
