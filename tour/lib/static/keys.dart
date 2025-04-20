f02b7b37abb8440240f56b9e52496f6d30c9e200
System Administrator <root@sofias-Mac.local>
2025-04-20 13:17:57 -0700

    podfile

--- tour/lib/static/keys.dart
+++ tour/lib/static/keys.dart
@@ -3,7 +3,7 @@
 class Keys {
   Keys._();
 
-  static const airtable = "";
-  static const googleMaps = "";
+  static const airtable = "patnOVix5R5wsTC.3ae61442e13ce197b8a0a10f553fc187973dcad80cc236efc7457c41d13ef3d2	";
+  static const googleMaps = "AIzaSyCWhUgriTLFdHxaj5g1N4z-4OR9hSj2c7Q";
 
 }
